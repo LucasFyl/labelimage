36dd4a7d3a90c4dda0aa472da30e7d691ca613d4
Lucas F <lfayolle1@gmail.com>
2015-03-25 11:34:06 +0100

    bg absolute + new 2logo + slower anim

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,15 +3,18 @@
 var controller = new ScrollMagic.Controller();
 // 2. Timeline
 var tmax_tl = new TimelineMax({pause:true});
-    tmax_tl.to('.landing h1', 1, {opacity:0,ease:Power2.easeOut}, "+=0.5");
-    tmax_tl.to('.background', 5, {top:'-50%',ease:Power1.easeInOut}, "-=1");
-    tmax_tl.to('header li a', 1, {color:'#000',ease:Power2.easeInOut}, "+=3");
+    tmax_tl.to('.landing h1', 2.5, {opacity:0,ease:Power4.easeInOut}, "+=0.5");
+    // tmax_tl.to('.background', 5, {top:'-50%',ease:Power1.easeInOut}, "-=1");
+    tmax_tl.to('header li a', 1, {color:'#000',ease:Power2.easeInOut,delay:5}, "+=10");
+    tmax_tl.to('header a.logo', 0.5, {className:'+=blk',ease:Power2.easeInOut}, "+=1");
  
 function landingAnim() {
-	var config = {};
+	var config = {pause:true};
 	var landingAnim = new TimelineMax(config);
-	landingAnim
-	// .to('.landing h1, .arrow-down', 0.6, {opacity:0,ease:Power2.easeOut})
-	.to('.main-content', 1, {top:0,ease:Power4.easeInOut}, "-=0.9")
-	.play().timeScale(0.6);
+	landingAnim.to('.landing h1', 1, {opacity:0,ease:Power2.easeOut}, "+=0.5");
+    landingAnim.to('.background', 5, {top:'-50%',ease:Power1.easeInOut}, "-=1");
+    landingAnim.to('header li a', 1, {color:'#000',ease:Power2.easeInOut}, "+=3");
+    landingAnim.to('header a.logo', 0.5, {className:'+=blk',ease:Power2.easeInOut}, "+=3");
+
+	landingAnim.play().timeScale(0.6);
 }
