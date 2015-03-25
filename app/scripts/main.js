52b3d0e01f2cf8d53958ceb7d291dcce0daaffcf
Lucas F <lfayolle1@gmail.com>
2015-03-25 13:06:27 +0100

    animation avec header bancale

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,11 +3,14 @@
 var controller = new ScrollMagic.Controller();
 // 2. Timeline
 var tmax_tl = new TimelineMax({pause:true});
-    tmax_tl.to('.landing h1', 2.5, {opacity:0,ease:Power4.easeInOut}, "+=0.5");
+    tmax_tl.to('.landing h1', 7, {opacity:0,ease:Power4.easeInOut}, "+=0.5");
     // tmax_tl.to('.background', 5, {top:'-50%',ease:Power1.easeInOut}, "-=1");
-    tmax_tl.to('header li a', 1, {color:'#000',ease:Power2.easeInOut,delay:5}, "+=10");
+    tmax_tl.to('header li a', 1, {color:'#000',ease:Power2.easeInOut,delay:5}, "+=5");
     tmax_tl.to('header a.logo', 0.5, {className:'+=blk',ease:Power2.easeInOut}, "+=1");
  
+
+
+
 function landingAnim() {
 	var config = {pause:true};
 	var landingAnim = new TimelineMax(config);
