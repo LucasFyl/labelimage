733bb54c0850adae168a0aec6e548002f07451e7
Lucas F <lfayolle1@gmail.com>
2015-04-30 15:24:50 +0200

    anim intro prestations

--- app/scripts/hello.js
+++ app/scripts/hello.js
@@ -1,10 +1,15 @@
 $(document).ready(function(){
-
+	TweenMax.set('header', {y:'-45px'});
 	TweenMax.to('.fadeIn', 0.25, {opacity:1,ease:Power1.easeOut,delay:0.1});
-	TweenMax.staggerTo(['nav li.anim-from-bottom'], 0.25, {marginTop:0,opacity:1,ease:Power1.easeOut,delay:0.3}, 0.15);
-	TweenMax.to('h1.anim-from-bottom', 0.25, {bottom:245,opacity:1,ease:Power1.easeOut,delay:0.4});
-	TweenMax.staggerTo('nav ul li span', 0.5, {opacity:1,top:16,ease:Power2.easeOut,delay:0.6});
-
 	
+	var animHeader = new TimelineMax({paused:true});
+	animHeader.set('header', {backgroundColor:'#f6f2ef'})
+			  .to('header', 0.25, {y:0,ease:Power2.easeOut,delay:0.3})
+			  .staggerTo(['nav li.anim-from-bottom'], 0.25, {marginTop:0,opacity:1,ease:Power1.easeOut}, 0.15)
+			  .to('h1.anim-from-bottom', 0.25, {bottom:245,opacity:1,ease:Power1.easeOut})
+			  .staggerTo('nav ul li span', 0.5, {opacity:1,top:16,ease:Power2.easeOut})
+			  .to('.loader', 0.5, {opacity:0,ease:Power1.easeInOut}, "-=0.5")
+			  .set('.loader', {display:'none'});
 
+	animHeader.play();
 });
