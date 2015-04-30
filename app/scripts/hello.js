168dc20ec170b84e0e5f3036b2c552c6de9ce75a
Lucas F <lfayolle1@gmail.com>
2015-04-30 15:00:32 +0200

    page prestations done

--- app/scripts/hello.js
+++ app/scripts/hello.js
@@ -5,28 +5,6 @@ $(document).ready(function(){
 	TweenMax.to('h1.anim-from-bottom', 0.25, {bottom:245,opacity:1,ease:Power1.easeOut,delay:0.4});
 	TweenMax.staggerTo('nav ul li span', 0.5, {opacity:1,top:16,ease:Power2.easeOut,delay:0.6});
 
-	new ScrollMagic.Scene({
-        triggerElement: '#trigger2',
-        triggerHook: 0,
-        duration: '100%'
-    })
-	.setTween(TweenMax.to('.landing .background', 0.5, {top:'-75%',ease:Linear.easeNone}))
-	// .addIndicators({name: 'background'})
-	.addTo(controller);
-
-	new ScrollMagic.Scene({
-        triggerElement: '#trigger3',
-        triggerHook: 0,
-        duration: 45
-    })
-	.setTween(TweenMax.to('header', 0.25, {top:'-=45',ease:Linear.easeNone}))
-	// .addIndicators({name: 'header'})
-	.addTo(controller);
-
-
-	$('.container-fluid').on("click", "a.arrow-down",function(){
-		var wH = $(window).height();
-		TweenLite.to(window, 1, {scrollTo:{y:wH}, ease:Power2.easeInOut});
-	});
+	
 
 });
---
+++ app/scripts/prestation.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+	
+	new ScrollMagic.Scene({
+        triggerElement: '#trigger2',
+        triggerHook: 0,
+        duration: '100%'
+    })
+	.setTween(TweenMax.to('.landing .background', 0.5, {top:'-75%',ease:Linear.easeNone}))
+	// .addIndicators({name: 'background'})
+	.addTo(controller);
+
+	new ScrollMagic.Scene({
+        triggerElement: '#trigger3',
+        triggerHook: 0,
+        duration: 45
+    })
+	.setTween(TweenMax.to('header', 0.25, {top:'-=45',ease:Linear.easeNone}))
+	// .addIndicators({name: 'header'})
+	.addTo(controller);
+
+
+	$('.container-fluid').on("click", "a.arrow-down",function(){
+		var wH = $(window).height();
+		TweenLite.to(window, 1, {scrollTo:{y:wH}, ease:Power2.easeInOut});
+	});
+	
+});
