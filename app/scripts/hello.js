3903a26c9ed839f90ca05b29ffcc7c5f6fdf8fbd
Lucas F <lfayolle1@gmail.com>
2015-05-02 13:36:42 +0200

    new assets + header typo

--- app/scripts/hello.js
+++ app/scripts/hello.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 			  .to('header', 0.25, {y:0,ease:Power2.easeOut,delay:0.3})
 			  .staggerTo(['nav li.anim-from-bottom'], 0.25, {marginTop:0,opacity:1,ease:Power1.easeOut}, 0.15)
 			  .to('h1.anim-from-bottom', 0.25, {bottom:245,opacity:1,ease:Power1.easeOut})
-			  .staggerTo('nav ul li span', 0.5, {opacity:1,top:16,ease:Power2.easeOut})
+			  .staggerTo('nav ul li span', 0.5, {opacity:1,top:18,ease:Power2.easeOut})
 			  .to('.loader', 0.5, {opacity:0,ease:Power1.easeInOut}, "-=0.5")
 			  .set('.loader', {display:'none'});
 
