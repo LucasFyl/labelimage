c05a49f056e0086e2cc7cb5f3af89801cfebe47a
Lucas F <lfayolle1@gmail.com>
2015-05-27 21:40:45 +0200

    adding in asset for labelnews

---
+++ www/site/accounts/admin.php
@@ -0,0 +1,11 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: admin
+email: lucas.fayolle@orange.fr
+password: >
+  $2a$10$qZ.C38Qnq2T8jOdZoadhaOd2uYODbnMWwYoj5ywRYnur/LYtBJlIO
+language: fr
+role: admin
+token: 6afc8fb85a8af4a1c03ffcd7214682bf68da00fe
+history:
+  - news
