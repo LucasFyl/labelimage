e04de3d0d3ab0dec425b47149f5dacca8334f1d2
Lucas F <lfayolle1@gmail.com>
2015-05-31 18:51:25 +0200

    labelnews responsive ok

--- www/site/snippets/news.php
+++ www/site/snippets/news.php
@@ -5,7 +5,7 @@
     <article class="col-xs-12 anews">
       <div class="row">
   
-        <div class="infos col-xs-12 col-sm-7">
+        <div class="infos col-sm-12 col-md-7">
           <h2 class="h1"><a href="<?php echo $thenews->url() ?>"><?php echo $thenews->title()->html() ?></a></h2>
           <h3 class="h2"><a href="<?php echo $thenews->url() ?>"><?php echo $thenews->title2()->html() ?></a></h3>
           <h4 class="h3"><a href="<?php echo $thenews->url() ?>"><?php echo $thenews->title3()->html() ?></a></h4>
@@ -13,7 +13,7 @@
           <a class="h3" href="<?php echo $thenews->url() ?>">Suite de l'article&nbsp;→</a>
         </div>
         
-        <div class="thumb col-xs-12 col-sm-5">
+        <div class="thumb col-sm-12 col-md-5">
           <?php if($image = $thenews->images()->sortBy('sort', 'asc')->first()): ?>
           <figure>
                 <img src="<?php echo $image->url() ?>" alt="<?php echo $page->title()->html() ?>">
