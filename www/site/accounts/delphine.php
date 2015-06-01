359cd3393ff5cef956ede6e2bf063018f91d0d02
Lucas F <lfayolle1@gmail.com>
2015-06-01 14:10:24 +0200

    admin account info

---
+++ www/site/accounts/delphine.php
@@ -0,0 +1,10 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: delphine
+firstname: Delphine
+lastname: LabelImage
+email: delphine@atelierlabelimage.com
+password: >
+  $2a$10$nVd4wm2ym2IavE.ViJxBm.AHhmXugRRkZnGMtoHZDSX5jAwkE9tpW
+language: fr
+role: admin
