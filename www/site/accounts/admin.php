f4a9339bc982385503263878da74602a551a8de5
Lucas Fayolle <lfayolle1@gmail.com>
2015-08-28 14:49:12 +0200

    first deployment

--- www/site/accounts/admin.php
+++ www/site/accounts/admin.php
@@ -6,10 +6,9 @@ password: >
   $2a$10$qZ.C38Qnq2T8jOdZoadhaOd2uYODbnMWwYoj5ywRYnur/LYtBJlIO
 language: fr
 role: admin
-token: 394a0ef1034e9e51e56fdb07967193f6b7977770
 history:
+  - null
   - home
   - tarifs
   - prestations
   - news/stephan-zaubitzer
-  - news
--- www/site/accounts/delphine.php
+++ www/site/accounts/delphine.php
@@ -5,6 +5,7 @@ firstname: Delphine
 lastname: LabelImage
 email: delphine@atelierlabelimage.com
 password: >
-  $2a$10$nVd4wm2ym2IavE.ViJxBm.AHhmXugRRkZnGMtoHZDSX5jAwkE9tpW
+  $2a$10$Sm2oMCeAtuBV1hIh3Tw18OCbwrX2I0bN.coVUWYP5oB0E2DHJ3wIy
 language: fr
 role: admin
+token: 95f68961edd7682bb94dbbb8afdca22033f154a2
